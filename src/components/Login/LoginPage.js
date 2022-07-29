06d6ace9af128dbf7ba5ef9977898002c9aaabbf
Sömen <somendaniel7@gmail.com>
2022-07-29 15:10:00 +0200

    creating basic registration and login page

---
+++ src/components/Login/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <>
+      <h1>login</h1>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/Registration/RegistrationPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RegistrationPage = () => {
+  return (
+    <>
+      <h1>registration</h1>
+    </>
+  );
+};
+
+export default RegistrationPage;
