f68bfea23ab4a2afefaaee0bc644557d78d51584
Sömen <somendaniel7@gmail.com>
2022-07-29 15:11:50 +0200

    creating basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import { writeUserData } from "./database/firebase";
-
+// import {
+//   getUsersData,
+//   writeUserData,
+//   testUsersData,
+//   readOnce,
+//   writeNewPost,
+//   deleteData,
+// } from "./database/firebase-config";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
+import RegistrationPage from "./components/Registration/RegistrationPage";
+import LoginPage from "./components/Login/LoginPage";
 function App() {
-  const test = () => {
-    writeUserData("anyadpicshaja", "zsolti", "zsolti@afereg.szenny");
-  };
+  //   const test = () => {
+  //     writeUserData("anyadpicsaja", "geci", "zsolti@afereg.szenny");
+  //   };
+
+  //   const testos = () => {
+  //     getUsersData();
+  //     //testUsersData();
+  //   };
+
+  //   const egyszer = () => {
+  //     readOnce();
+  //   };
+
+  //   const update = () => {
+  //     writeNewPost("anyadpicsahja", "parasztlengő", "penisz@pumpa.com");
+  //   };
+
+  //   const deleteDa = () => {
+  //     deleteData("/test/anyadpicsajha");
+  //   };
 
   return (
     <>
-      <h1>szoszimo</h1>
-      <button onClick={test}>tesztoszteron</button>
+      <Router>
+        <Routes>
+          <Route path="/registration" element={<RegistrationPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/" element={<WelcomePage />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
